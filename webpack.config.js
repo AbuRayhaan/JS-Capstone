da4d7dbed63c1e5f18a691d3ce7c0fbc202afc66
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-16 00:17:59 +0100

    Modified the webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -32,11 +32,6 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        use : [
-          {
-            loader: 'file loader',
-          },
-        ],
       },
     ],
   },
