3a2400acc266c2cb06ccbd1ebe1cefeb05ac208d
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-21 12:47:12 +0100

    Modified the index.js file

--- src/index.js
+++ src/index.js
@@ -39,46 +39,53 @@ const loadShow = async () => {
       </div>
     `;
     catalog.appendChild(movie);
-  }
 
-  const commentPopup = document.querySelector('commentPopup');
-  const commentBtn = document.querySelectorAll('.commentBtn');
+    const commentPopup = document.querySelector('.commentPopup');
+    const commentBtn = document.querySelectorAll('.commentBtn');
 
-  commentBtn.forEach((btn, i) => {
-    btn.addEventListener('click', () => {
-      const popUp = document.createElement('div');
-      // popUp.setAttribute('class', 'popUpComment')
-      popUp.classList.add('active');
-      // catalogPop.classList.add('active');
-      popUp.innerHTML = `
-      <div class="catalog-popUp d-flex">
-        <i class="fa-solid fa-xmark"></i>
-        <div class="movie-image hide1">
-            <img class="img-pop" src = "${showList[i].image.original}" alt = "${showList[i].name}">
-        </div>
+    commentBtn.forEach((btn, i) => {
+      btn.addEventListener('click', () => {
+        catalog.classList.add('active');
+        const popUp = document.createElement('div');
+        commentPopup.innerHTML = '';
+        popUp.innerHTML = `
+        <div class="catalog-popUp d-flex">
+          <i class="fa-solid fa-xmark"></i>
+          <div class="movie-image hide1">
+              <img class="img-pop" src = "${showList[i].image.original}" alt = "${showList[i].name}">
+          </div>
+  
+          <div class="movie-info-pop d-flex">
+              <p>${showList[i].summary}</p>
+              <h3>COMMENTS</h3>
+              <small class="commentCount">20 comments</small>
+              <ul class="commentList">
+                  <li>User1: Comment1</li>
+                  <li>User2: Comment2</li>
+                  <li>User3: Comment3</li>
+                  <li>User4: Comment4</li>
+                  <li>User5: Comment5</li>
+              </ul>
+              <form action="" class="inputComment d-flex">
+                  <input class="username" type="text" name="" value="" placeholder="Your Name">
+                  <input class="comment" type="text" name="" value="" placeholder="Your Comment">
+                  <small class="msg"></small>
+                  <button class="submitBtn">submit</button>
+              </form>
+          </div>
+        </div>`
+        commentPopup.appendChild(popUp);
 
-        <div class="movie-info-pop d-flex">
-            <p>${showList[i].summary}</p>
-            <h3>COMMENTS</h3>
-            <small class="commentCount">20 comments</small>
-            <ul class="commentList">
-                <li>User1: Comment1</li>
-                <li>User2: Comment2</li>
-                <li>User3: Comment3</li>
-                <li>User4: Comment4</li>
-                <li>User5: Comment5</li>
-            </ul>
-            <form action="" class="inputComment d-flex">
-                <input class="username" type="text" name="" value="" placeholder="Your Name">
-                <input class="comment" type="text" name="" value="" placeholder="Your Comment">
-                <small class="msg"></small>
-                <button class="submitBtn">submit</button>
-            </form>
-        </div>
-      </div>`
-      body.append(popUp);
+        const closePopup = document.querySelector('.fa-xmark');
+  
+        closePopup.addEventListener('click', () => {
+          console.log('click works');
+          commentPopup.innerHTML = '';
+          catalog.classList.remove('active');
+        });
+      });
     });
-  });
+  }
 }
 
 loadShow();
